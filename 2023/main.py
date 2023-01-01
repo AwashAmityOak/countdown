62fc0ea1081fabc42dbaa5412675693582302ff6
AwashAmityOak <117988946+AwashAmityOak@users.noreply.github.com>
2023-01-01 10:14:27 +0900

    Created 2023 version

---
+++ 2023/main.py
@@ -0,0 +1,22 @@
+import datetime
+
+import pyxel
+
+class App():
+	def __init__(self):
+		pyxel.init(128, 64, title="Countdown")
+		self.t = 0
+		pyxel.run(self.update, self.draw)
+	def update(self):
+		self.t = datetime.datetime(year=2024, month=1, day=1).timestamp() - \
+			datetime.datetime.now().timestamp()
+		self.string = str(int(self.t)) if 0.0 < self.t else "Happy New Year!"
+	def draw(self):
+		pyxel.cls(0)
+		pyxel.text(
+			(pyxel.width - len(self.string) * pyxel.FONT_WIDTH) // 2, 
+			(pyxel.height - pyxel.FONT_WIDTH) // 2, 
+			self.string, pyxel.COLOR_YELLOW
+		)
+
+App()
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Countdown
 
 This is my countdown display program made with [Pyxel](https://github.com/kitao/pyxel).
+
++ [New Year's Eve 2022 version](https://awashamityoak.github.io/countdown/2022)
++ [New Year's Eve 2023 version](https://awashamityoak.github.io/countdown/2023)
